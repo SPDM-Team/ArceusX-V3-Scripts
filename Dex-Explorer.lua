c5a9d2955b4c7b0d9be86761bf91a2ed62cf44e4
Riky47 <56732510+Riky47@users.noreply.github.com>
2023-03-16 21:13:57 +0100

    Add files via upload

---
+++ Dex-Explorer.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))()
+--loadstring(game:HttpGet("https://gist.githubusercontent.com/DinosaurXxX/b757fe011e7e600c0873f967fe427dc2/raw/ee5324771f017073fc30e640323ac2a9b3bfc550/dark%2520dex%2520v4"))(); -- Dark Dex v4
